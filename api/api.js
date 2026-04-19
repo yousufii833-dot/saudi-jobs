5be6898fdb77ae2a894e2eb846ac25fb8951648b
yousufii833-dot <yousufii833@gmail.com>
2026-04-19 20:29:06 +0300

    Change access level from public to private

--- api/api.js
+++ api/api.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
             ...data,
             time: new Date().toISOString()
         }), {
-            access: 'public',
+            access: 'private',
             addRandomSuffix: false,
         });
         
